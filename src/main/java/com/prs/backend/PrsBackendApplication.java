bd63726a6697e770fca9f1232abb01b60990a891
Abdirahman <abdirahmandubow4@gmail.com>
2026-07-08 19:51:56 +0800

    Add application configuration and security setup

---
+++ src/main/java/com/prs/backend/PrsBackendApplication.java
@@ -0,0 +1,12 @@
+package com.prs.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrsBackendApplication.class, args);
+    }
+}
