93730ed2fc1b6f7650d58e035eb598854dde8228
Abdirahman <abdirahmandubow4@gmail.com>
2026-07-08 19:58:16 +0800

    Add project test classes

---
+++ src/test/java/com/prs/backend/PrsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prs.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
